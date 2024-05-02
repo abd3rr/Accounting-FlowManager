55319c2ba9fcadf5c3589e3440184b577336e284
Abdurrahman <59231440+abd3rr@users.noreply.github.com>
2024-05-02 15:19:53 +0200

    project env init

---
+++ Accounting-FlowManager/src/main/java/fr/uha/AccountingFlowManager/AccountingFlowManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.uha.AccountingFlowManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingFlowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingFlowManagerApplication.class, args);
+	}
+
+}
---
+++ Accounting-FlowManager/src/test/java/fr/uha/AccountingFlowManager/AccountingFlowManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uha.AccountingFlowManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingFlowManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
