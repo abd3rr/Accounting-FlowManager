ef63c5e006ff03f0c4f66dc7aaf04cebcc5785eb
Oreste Muhirwa Gabo <muhirwa.g.oreste@gmail.com>
2024-05-06 10:20:27 +0200

    Content to fill the page

--- Accounting-FlowManager/src/main/java/fr/uha/AccountingFlowManager/controller/PageController.java
+++ Accounting-FlowManager/src/main/java/fr/uha/AccountingFlowManager/controller/PageController.java
@@ -4,27 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class PageController {
 
+    private void setActivePage(Model model, String page) {
+        model.addAttribute(page, true);
+    }
+
     @GetMapping({"/","/index"})
     public String index(Model model) {
+        setActivePage(model, "home");
         return "home";
     }
+
     @GetMapping("/about")
     public String about(Model model) {
+        setActivePage(model, "about");
         return "about";
     }
     @GetMapping("/help")
     public String help(Model model) {
+        setActivePage(model, "help");
         return "help";
     }
     @GetMapping("/terms")
     public String terms(Model model) {
+        setActivePage(model,  "terms");
         return "terms";
     }
     @GetMapping("/services")
     public String services(Model model) {
+        setActivePage(model,  "services");
         return "services";
     }
 }
